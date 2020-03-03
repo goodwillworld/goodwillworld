b52e0b4e5f8aa6bd43bf6abaa8776e9eb20eefd0
Rong.fan <844655188@qq.com>
2020-03-03 17:02:41 +0800

    Initial version of jboth generated by JHipster-6.7.1

---
+++ src/main/java/com/jboth/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.jboth.repository;
+
+import com.jboth.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
